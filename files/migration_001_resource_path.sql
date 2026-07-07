3f46338d800f298d0d77ef57f2dcba6df4e75bab
Thierry <thierclaude@gmail.com>
2026-07-07 09:33:26 +0200

    Add folk guitar app icon

--- files/migration_001_resource_path.sql
+++
@@ -1,4 +0,0 @@
--- Migration : ajout d'un champ "chemin / lien des supports" par élément
--- À exécuter avec : node run-schema.js <TURSO_DATABASE_URL> <TURSO_AUTH_TOKEN> migration_001_resource_path.sql
-
-ALTER TABLE work_items ADD COLUMN resource_path TEXT;
